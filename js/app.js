35667e846efb7144281da3ce9b2a7d9d1ce1b9d1
kacper-ragankiewicz <81741652+kacper-ragankiewicz@users.noreply.github.com>
2021-04-05 05:34:36 +0200

    add dote

--- js/app.js
+++ js/app.js
@@ -16,14 +16,14 @@ carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 nextBtn.addEventListener('click', ()=>{
     if (counter >= carouselImages.length -1) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    carouselSlide.style.transition = "transform 0.6s ease-in-out";
     counter++;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
 
 prevBtn.addEventListener('click', ()=>{
     if (counter <= 0)return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    carouselSlide.style.transition = "transform 0.6s ease-in-out";
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
